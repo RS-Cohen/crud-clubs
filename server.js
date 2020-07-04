6e6bb5a93c804e89226a9c97c958504296b8fd8a
RS-Cohen <rocohen84@gmail.com>
2020-07-04 17:58:41 -0300

    .

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
